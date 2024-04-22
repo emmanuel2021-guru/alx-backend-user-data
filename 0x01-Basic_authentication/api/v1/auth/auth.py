289086eb1b4452841369fdd8a6ed36c6d0687f28
emmanuel <emmanuellawakin2005@gmail.com>
2024-04-22 22:23:36 +0100

    Completed task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+from flask import request
+from typing import List, TypeVar
+
+class Auth():
+    """
+    Template for all authentication sytem to be implemented
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check if auth is required
+        """
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        """ define the authorization header
+        """
+        return None
+    
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns the current user
+        """
+        return None
